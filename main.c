292b96f19c5b9e27a25682cba56a9c08b8c0eba8
Wojciech Gościcki <39399945+goscickiw@users.noreply.github.com>
2018-11-08 12:44:24 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 
 int main (void)
 {
-  printf ("Wybierz rodzaj operacji:\nDodawanie - 1\nOdejmowanie - 2\nMnozenie - 3\nDzielenie - 4\nPierwiastkowanie - 5\nPotegowanie - 6\n:);
+  
+  printf ("Wybierz rodzaj operacji:\nDodawanie - 1\nOdejmowanie - 2\nMnozenie - 3\nDzielenie - 4\nPierwiastkowanie - 5\nPotegowanie - 6\n:); 
+  unsigned char operacja;
+  scanf ("%d", operacja);
   
   printf ("Test successful.");
   getch ();
