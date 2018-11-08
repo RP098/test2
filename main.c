df270ce4361f9fc1376d295ecf3b0bea2b160895
RP098 <44466509+RP098@users.noreply.github.com>
2018-11-08 12:46:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,10 @@ int main (void)
   printf ("Wybierz rodzaj operacji:\nDodawanie - 1\nOdejmowanie - 2\nMnozenie - 3\nDzielenie - 4\nPierwiastkowanie - 5\nPotegowanie - 6\n:); 
   unsigned char operacja;
   scanf ("%d", operacja);
-  
+  switch(operacja)
+          {
+            
+          }
   printf ("Test successful.");
   getch ();
 }
