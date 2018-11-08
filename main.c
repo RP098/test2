d4e6d5ad1c1b205de22992c7f31fa80adadd5f63
Wojciech Gościcki <39399945+goscickiw@users.noreply.github.com>
2018-11-08 12:37:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 int main (void)
 {
+  printf ("Wybierz rodzaj operacji:\nDodawanie - 1\nOdejmowanie - 2\nMnozenie - 3\nDzielenie - 4\nPierwiastkowanie - 5\nPotegowanie - 6\n:);
+  
   printf ("Test successful.");
   getch ();
 }
